04bf367fd5d18a6bbdc37d42f1b064350e32643e
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2017-11-20 15:59:45 +0200

    Add some tests

--- rust-fmi-course/solutions/02/tests/my_tests.rs
+++ rust-fmi-course/solutions/02/tests/my_tests.rs
@@ -2,32 +2,84 @@ extern crate solution;
 use solution::Polynomial;
 
 #[test]
-fn test_description_constraints() {
+fn test_simple_functions() {
+    assert_eq!(Polynomial::from(vec![]), Polynomial::from(vec![]));
     assert_eq!(Polynomial::from(vec![]), Polynomial::from(vec![0.0]));
     assert_eq!(Polynomial::from(vec![]), Polynomial::from(vec![0.0, 0.0]));
     assert_eq!(Polynomial::from(vec![0.0, 1.0]), Polynomial::from(vec![1.0]));
-    assert_eq!(Polynomial::from(vec![1.0, 2.0, 3.0]), Polynomial::from(vec![0.0, 0.0, 1.0, 2.0, 3.0]));
+    assert_eq!(
+        Polynomial::from(vec![1.0, 2.0, 3.0]),
+        Polynomial::from(vec![0.0, 0.0, 1.0, 2.0, 3.0])
+    );
 
+    let p = Polynomial::from(vec![2.0, 0.0, 1.0, 7.0]);
+    assert!(p.has(&(0.0, 7.0)));
+    assert!(! p.has(&(-1.0, 1.0)));
 
     assert_eq!(
-        Polynomial::from(vec![1.0, 2.0]) * Polynomial::from(vec![2.0, 1.0]),
-        Polynomial::from(vec![2.0, 5.0, 2.0])
+        Polynomial::from(vec![1.0, 2.0, 3.0]) * 2.0,
+        Polynomial::from(vec![2.0, 4.0, 6.0])
+    );
+    assert_eq!(
+        Polynomial::from(vec![1.0, 2.0, 3.0]) * 0.0,
+        Polynomial::from(vec![])
+    );
+
+    assert_eq!(p.clone()/1.0, p);
+    assert_eq!(
+        Polynomial::from(vec![1.0, 2.0, 3.0]) / 2.0 ,
+        Polynomial::from(vec![0.5, 1.0, 1.5])
     );
 }
 
 
 #[test]
-fn test_while_implementing() {
-    let p = Polynomial::from(vec![2.0, 0.0, 1.0, 7.0]);
-    assert!(p.has(&(0.0, 7.0)));
-    assert!(! p.has(&(-1.0, 1.0)));
-    /*
-    println!("Result 01: {:?}", Polynomial::from(vec![]));
-    println!("Result 02: {:?}", Polynomial::from(vec![0.0]));
-    println!("Result 03: {:?}", Polynomial::from(vec![0.0, 0.0]));
-    println!("Result 04: {:?}", Polynomial::from(vec![1.2, 0.0]));
-    println!("Result 05: {:?}", Polynomial::from(vec![0.0, 1.3]));
-    println!("Result 06: {:?}", Polynomial::from(vec![1.4]));
-    println!("Result 07: {:?}", Polynomial::from(vec![0.0, 0.0, 1.0, 2.0, 3.0]));
-    */
+fn test_addition_multiplication() {
+    assert_eq!(
+        Polynomial::from(vec![1.0, 2.0]) + Polynomial::from(vec![-1.0, -2.0]),
+        Polynomial::from(vec![0.0])
+    );
+
+    assert_eq!(
+        Polynomial::from(vec![1.0, 2.0, 3.0]) + Polynomial::from(vec![4.0, 5.0]),
+        Polynomial::from(vec![1.0, 6.0, 8.0])
+    );
+
+    assert_eq!(
+        Polynomial::from(vec![1.0, 2.0]) * Polynomial::from(vec![2.0, 1.0]),
+        Polynomial::from(vec![2.0, 5.0, 2.0])
+    );
+
+    assert_eq!(
+        Polynomial::from(vec![1.0, 2.0]) * Polynomial::from(vec![0.0]),
+        Polynomial::from(vec![0.0])
+    );
+}
+
+#[test]
+fn test_interpolation() {
+    assert_eq!(
+        Polynomial::interpolate(vec! {
+            ( 1.0, 1.0),
+            ( 1.0, 4.0),
+            ( 3.0, 9.0),
+        }),
+        None
+    );
+    assert_eq!(
+        Polynomial::interpolate(vec! {
+            ( 1.0, 1.0),
+            ( 2.0, 4.0),
+            ( 3.0, 9.0),
+        }).unwrap(),
+        Polynomial::from(vec![1.0, 0.0, 0.0])
+    );
+    assert_eq!(
+        Polynomial::interpolate(vec! {
+            ( 1.0, 1.0),
+            ( 2.0, 8.0),
+            ( 3.0, 27.0),
+        }).unwrap(),
+        Polynomial::from(vec![6.0, -11.0, 6.0])
+    );
 }
