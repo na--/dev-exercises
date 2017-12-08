b47a277dfd26d0451b28d4f441aab3972b77f7e9
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2017-12-08 15:53:00 +0200

    Add some simple tests, others have contributed more

--- rust-fmi-course/solutions/03/tests/my_tests.rs
+++ rust-fmi-course/solutions/03/tests/my_tests.rs
@@ -1,6 +1,30 @@
 extern crate solution;
+use solution::*;
+
+macro_rules! assert_match {
+    ($pattern:pat, $actual:expr) => {
+        if let $pattern = $actual {
+            assert!(true);
+        } else {
+            assert!(false, "Expected {} to match {:?}", stringify!($pattern), $actual);
+        }
+    }
+}
 
 #[test]
-fn simple_test() {
-    assert_eq!(4, 2 + 2);
+fn command_test() {
+    assert_match!(Ok(Command::Help), "h".parse::<Command>());
+    assert_match!(Ok(Command::Info), "i".parse::<Command>());
+    assert_match!(Ok(Command::Info), "in".parse::<Command>());
+    assert_match!(Ok(Command::Quit), "q".parse::<Command>());
+    assert_match!(Ok(Command::Quit), "qu".parse::<Command>());
+    assert_match!(
+        Ok(Command::TryLetter('x')),
+        "try letter x".parse::<Command>()
+    );
+    assert_match!(Ok(Command::TryWord(_)), "try word xyzzy".parse::<Command>());
+    assert_match!(Ok(Command::TryLetter('x')), "t l x".parse::<Command>());
+
+
+    assert_match!(Err(GameError::ParseError(_)), "z".parse::<Command>());
 }
